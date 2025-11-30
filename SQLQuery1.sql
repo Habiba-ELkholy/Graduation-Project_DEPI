613214778d435fdb3dd799cc0849c715a8ec0458
SotirMilad <sotirmilad@gmail.com>
2025-11-30 23:11:01 +0200

    update
    update

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,3 @@
-﻿-- دي رسالة وهمية جاية من "شخص تاني" (رقم 2) ليكي إنتي (رقم 1)
+﻿
 INSERT INTO Messages (SenderId, ReceiverId, Content, Timestamp) 
 VALUES (2, 1, 'اهلا و سهلا بحضرتك، اقدر اساعدك ازاي؟', GETDATE());

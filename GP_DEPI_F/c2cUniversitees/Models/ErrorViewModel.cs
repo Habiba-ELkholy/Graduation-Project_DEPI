f172d3d984f55e5c7111aae6e5d3ad36dd905a4c
SotirMilad <sotirmilad@gmail.com>
2025-11-30 23:06:48 +0200

    updates
    updates

--- GP_DEPI_F/c2cUniversitees/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace c2cUniversitees.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
